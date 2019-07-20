3936d273a521e180f18880c74d3de25cd1ff6d30
manzav95 <zavalamanuel95@gmail.com>
2019-07-20 15:48:58 -0700

    setup a updateGame() and created the random number from 1-12 for the gems. just a few minor tweaks left

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,48 +2,85 @@ $(document).ready(function(){
 
     let wins = 0;
     let losses = 0;
-    let totalScore = 0;
-    let randomNumber;
+    var totalScore = 0;
 
-    $("#redGem").on("click", function(){
-        let redNumber = Math.floor(Math.random() * 12) + 1;
-        
-
-        console.log("Red Number: " + redNumber);
-    })
-
-    $("#blueGem").on("click", function(){
-        alert("Blue");
-    })
-
-    $("#yellowGem").on("click", function(){
-        alert("Yellow");
-    })
-
-    $("#greenGem").on("click", function(){
-        alert("Green");
-    })
-
-    
-    //ask TA how this becomes a number starting from 19?
     function randomNumberGenerator(){
-        randomNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
-        $("#random-number").html(randomNumber);
-        console.log(randomNumber);
+    randomNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+    $("#random-number").html(randomNumber);
+    console.log(randomNumber);
     }
 
     function updateGame(){
+
         if(totalScore === randomNumber){
-            wins++
+            alert("WINNER!");
+            wins++;
             $("#wins").html(wins);
+            restartGame();
         }
+    
+        else if(totalScore > randomNumber){
+            alert("LOSER!");
+            losses++;
+            $("#losses").html(losses);
+            restartGame();
+        }
+    
     }
 
     function restartGame(){
+    totalScore = 0;
+
+    $("#total-score").html(totalScore);
+    randomNumberGenerator();
 
     }
 
     randomNumberGenerator();
+
+    let redNumber = Math.floor(Math.random() * 12) + 1;
+
+    let blueNumber = Math.floor(Math.random() * 12) + 1;
+
+    let yellowNumber = Math.floor(Math.random() * 12) + 1;
+
+    let greenNumber = Math.floor(Math.random() * 12) + 1;
+
+
+    $("#red-gem").on("click", function(){
+    $("#total-score").html(totalScore+=redNumber);
+
+    updateGame();
+
+    console.log("Red Number: " + redNumber);
+    })
+
+    $("#blue-gem").on("click", function(){
+    $("#total-score").html(totalScore+=blueNumber);
+
+    updateGame();
+
+    console.log("Blue Number: " + blueNumber);
+    })
+
+    $("#yellow-gem").on("click", function(){
+    $("#total-score").html(totalScore+=yellowNumber);
+
+    updateGame();
+
+    console.log("Yellow Number: " + yellowNumber);
+
+    })
+
+    $("#green-gem").on("click", function(){
+    $("#total-score").html(totalScore+=greenNumber);
+
+    updateGame();
+    
+    console.log("Green Number: " + greenNumber);
+
+    })
+
 })
 
 
