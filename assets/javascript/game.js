b2dbdef7dc5e3dc8f6e8d2d7fae751be08c8c56e
manzav95 <zavalamanuel95@gmail.com>
2019-07-20 17:41:58 -0700

    cleaned up game.js file and added comments as well to explain the program

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    //Declaring Variables
 
     let wins = 0;
     let losses = 0;
@@ -9,13 +10,12 @@ $(document).ready(function(){
     let yellowNumber = 0;
     let greenNumber = 0;
 
+    //Creates a random number value for the randomNumber and all 4 gems
     function randomNumberGenerator(){
-    randomNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
-    $("#random-number").html(randomNumber);
-    console.log(randomNumber);
-    }
+        randomNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+        $("#random-number").html(randomNumber);
 
-    function updateGems(){
+
         redNumber = Math.floor(Math.random() * 12) + 1;
 
         blueNumber = Math.floor(Math.random() * 12) + 1;
@@ -23,8 +23,13 @@ $(document).ready(function(){
         yellowNumber = Math.floor(Math.random() * 12) + 1;
 
         greenNumber = Math.floor(Math.random() * 12) + 1;
+
+        //Not needed for gameplay. Console.logs all random values
+        consoleLog();
     }
 
+
+    //These are the actions that will be taken when the randomNumber is met or is exceeded.
     function updateGame(){
 
         if(totalScore === randomNumber){
@@ -43,54 +48,47 @@ $(document).ready(function(){
     
     }
 
+    //This allows a new random number to be generated and resets user score back to 0
     function restartGame(){
     totalScore = 0;
 
     $("#total-score").html(totalScore);
 
     randomNumberGenerator();
-    updateGems();
 
     }
+    //These console.logs were used when developing the game to show values of random generated numbers
+    function consoleLog(){
+        console.log(randomNumber);
+        console.log("Red Number: " + redNumber);
+        console.log("Blue Number: " + blueNumber);
+        console.log("Yellow Number: " + yellowNumber);
+        console.log("Green Number: " + greenNumber);
+    }
 
+    //This is where the game first begins. Creating random values for all 4 gems and the randomNumber as well.
     randomNumberGenerator();
-    updateGems();
 
-
+    //On click functions for all gems. Updates the value of the totalScore
     $("#red-gem").on("click", function(){
     $("#total-score").html(totalScore+=redNumber);
-
     updateGame();
-
-    console.log("Red Number: " + redNumber);
     })
 
     $("#blue-gem").on("click", function(){
     $("#total-score").html(totalScore+=blueNumber);
-
     updateGame();
-
-    console.log("Blue Number: " + blueNumber);
     })
 
     $("#yellow-gem").on("click", function(){
     $("#total-score").html(totalScore+=yellowNumber);
-
     updateGame();
-
-    console.log("Yellow Number: " + yellowNumber);
-
     })
 
     $("#green-gem").on("click", function(){
     $("#total-score").html(totalScore+=greenNumber);
-
     updateGame();
-
-    console.log("Green Number: " + greenNumber);
-
     })
-
 })
 
 
